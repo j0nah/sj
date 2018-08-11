3bfa17fbf103802d2e6afd2b1f917c2aa9d97fd0
j0nah <contact@j0nah.com>
2018-08-11 00:00:57 -0700

    * add recursive function to find nested values * add wrapper function to trim up whiitespace

--- sj.go
+++ sj.go
@@ -1,16 +1,43 @@
 package sj
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+)
 
-func search(needle string, haystack string) (*string, error) {
-	// validate json
-	var j json.RawMessage
-	err := json.Unmarshal([]byte(haystack), &j)
-
+func Search(needle string, rawHaystack string) (*[]string, error) {
+	buffer := new(bytes.Buffer)
+	err := json.Compact(buffer, []byte(rawHaystack))
 	if err != nil {
 		return nil, err
 	}
 
-	var res string
-	return &res, nil
+	return search(needle, buffer.String())
+}
+
+func search(needle string, haystack string) (*[]string, error) {
+	if haystack == "" {
+		return nil, nil
+	}
+
+	var result []string
+	var decode map[string]json.RawMessage
+	err := json.Unmarshal([]byte(haystack), &decode)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range decode {
+		if k == needle {
+			result = append(result, string(v))
+		} else {
+			sr, err := search(needle, string(v))
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, *sr...)
+		}
+	}
+
+	return &result, nil
 }
--- sj_test.go
+++ sj_test.go
@@ -5,40 +5,70 @@ import (
 	"testing"
 )
 
-func TestInvalidJson(t *testing.T) {
-	haystack := `
-	{
-		"foo" : {
-			"baz" : "bob"
-		
-	}
-`
-	_, err := search("", haystack)
-
-	if err == nil {
-		t.Fatal(err.Error())
-	} else {
-		fmt.Println(err.Error())
-	}
-}
+//func TestInvalidJson(t *testing.T) {
+//	haystack := `
+//	{
+//		"foo" : {
+//			"baz" : "bob"
+//
+//	}
+//`
+//	_, err := search("", haystack)
+//
+//	if err == nil {
+//		t.Fatal(err.Error())
+//	} else {
+//		fmt.Println(err.Error())
+//	}
+//}
+//
+//func TestSimple(t *testing.T) {
+//	haystack := `
+//	{
+//		"foo" : {
+//			"baz" : "bob"
+//		}
+//	}
+//`
+//	needle := "baz"
+//	expected := `{"baz":"bob"}`
+//	actual, err := search(needle, haystack)
+//
+//	if err != nil {
+//		t.Fatal(err.Error())
+//	}
+//
+//	if expected != *actual {
+//		t.Errorf("Expected: %s Got: %s \n", expected, *actual)
+//	}
+//}
 
 func TestSimple(t *testing.T) {
 	haystack := `
-	{
-		"foo" : {
-			"baz" : "bob"
+		{
+			"baz":"bob",
+			"foo": {
+				"baz" : "gumble",
+				"lumble" : {
+					"baz" : {
+						"hello" : "world"
+					}
+				}
+			}
 		}
-	}
 `
 	needle := "baz"
-	expected := `{"baz":"bob"}`
-	actual, err := search(needle, haystack)
+	val, _ := Search(needle, haystack)
 
-	if err != nil {
-		t.Fatal(err.Error())
+	for _, v := range *val {
+		fmt.Println(v)
 	}
 
-	if expected != *actual {
-		t.Errorf("Expected: %s Got: %s \n", expected, *actual)
-	}
+	//if err != nil {
+	//	t.Fatal(err.Error())
+	//}
+	//
+	//if expected != *actual {
+	//	t.Errorf("Expected: %s Got: %s \n", expected, *actual)
+	//}
 }
